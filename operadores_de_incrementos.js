c88b52912f2ee6485f8042888f0706750a50de42
Dev-er <ubiratantomaz@gmail.com>
2022-09-16 17:18:01 -0300

    Atividades com operadores de incremento e decremento

---
+++ operadores_de_incrementos.js
@@ -0,0 +1,15 @@
+alert('Código Funcionando!!!') // mostra mensagem para o usuário
+const nomeDoUsuario = prompt('Olá, qual é o seu nome?')
+
+let num = Number(prompt('Olá, ' + nomeDoUsuario + '. Digite algum número por favor!! :)'))
+
+//console.log(typeoff num) // typeoff retorna o tipo de valor que a variável armazena
+
+//const sucessor = ++num
+//const antecessor = --num
+
+//alert('O sucessor de' + num + 'é ' + sucessor)
+//alert('O antecessor de' + num + 'é' + antecessor)
+
+console.log(num++)
+
